999f9ee3f1c990525345a33b0fcb092f8948a84f
AnoudAlshnakat <ano.o@live.com>
2021-02-25 11:33:06 +0100

    simple initlisation required to test LIC_0

--- main.c
+++ main.c
@@ -5,6 +5,22 @@
 int main()
 {
 
+  NUMPOINTS = 5;
+  PARAMETERS.LENGTH1 = 2;
+
+  X[0] = 5;
+  X[1] = 3;
+  X[2] = 5;
+  X[3] = 8;
+  X[4] = 6;
+
+  Y[0] = 9;
+  Y[1] = 7;
+  Y[2] = 10;
+  Y[3] = 11;
+  Y[4] = 11;
+
+
 
   return 0;
 }
