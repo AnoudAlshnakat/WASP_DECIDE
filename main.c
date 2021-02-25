41f8d39014fae9e960fbd55a752598388c70e9eb
AnoudAlshnakat <ano.o@live.com>
2021-02-25 21:05:32 +0100

    comment the initial test for LIC0

--- main.c
+++ main.c
@@ -2,27 +2,27 @@
 #include <stdio.h>
 #include <string.h>
 
+NUMPOINTS = 10;
 int main()
 {
-
-  NUMPOINTS = 5;
+   //LIC0
+  /*
+   NUMPOINTS = 5;
   PARAMETERS.LENGTH1 = 2;
-
   X[0] = 5;
   X[1] = 3;
   X[2] = 5;
   X[3] = 8;
   X[4] = 6;
-
   Y[0] = 9;
   Y[1] = 7;
   Y[2] = 10;
   Y[3] = 11;
   Y[4] = 11;
 
-  boolean a = LIC_0();
+  boolean a = LIC_0(PARAMETERS.LENGTH1);
 
-  if (a == 1)
+    if (a == 1)
   {
     printf("passed \n");
   }
@@ -30,6 +30,13 @@ int main()
   {
     printf("failed \n");
   }
+ 
+ */
+
+
+
+
+
 
   return 0;
 }
