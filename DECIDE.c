af5ef5371e3bf6f291b855b45449d3cf948b2d7a
AnoudAlshnakat <ano.o@live.com>
2021-02-25 21:09:18 +0100

    LIC_1 function initialisation

--- DECIDE.c
+++ DECIDE.c
@@ -44,4 +44,47 @@ double length(x1, y1, x2, y2)
 }
 
 
+boolean LIC_1(double p_rad)
+{
+    int i;
+    double len1,len2,len3;          //triengles sides length
+    double sp,a ;                   //semi parameter and area of the tringle
+    double measured_r ;             //measured radius
 
+
+    for (i = 0; i < (NUMPOINTS -2); i++)
+    {
+        //calculate the length which is the distance between all the points, to form a tringle
+            
+        len1 = length(X[i],Y[i],X[i+1],Y[i+1]); 
+        len2 = length(X[i+1],Y[i+1],X[i+2],Y[i+2]);
+        len3 = length(X[i+2],Y[i+2],X[i],Y[i]);
+    
+       //calculaute the semi-perimeter of the tringle
+        sp = (len1 + len2 + len3)/2;
+
+        //area of tringle
+        a = sqrt(sp*(sp-len1)*(sp-len2)*(sp-len3));
+
+        if (DOUBLECOMPARE(a, 0)==EQ) 
+        {
+            measured_r= fmax(len1,len2);
+            measured_r= fmax(measured_r,len2);
+        } 
+        else
+        {   //circle circumscribing a triangle is r = len1 len2 len3 / (4* tringle area)
+            measured_r= len1*len2*len3/(4*a);
+        } 
+         
+        if (DOUBLECOMPARE(measured_r, p_rad) == GT) 
+        {
+            printf("here 1 measured_r %f \n", measured_r);
+             return 1;
+        } 
+
+
+
+    }
+    printf("here 2 measured_r %f \n", measured_r);
+    return 0;
+}
