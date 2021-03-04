2a6c418a375a018380f11471ade768010defc977
AnoudAlshnakat <ano.o@live.com>
2021-03-04 13:36:34 +0100

    test: LIC0 failing case 2

--- test_requierments.c
+++ test_requierments.c
@@ -21,14 +21,35 @@ boolean LIC_0_Fail_case1()
     return result; 
 }
 
+boolean LIC_0_Fail_case2()
+{
+    //test inputs: Number of points = 10, the parameter LENGTH1 = -3 unit, and coordinates of 10 points.
+    NUMPOINTS = 10;
+    double X[] = {5, 3, 5, 8, 5, 3, 5, 8, 2, 3};
+    double Y[] = {1, 4, 3, 1, 5, 6, 1, 6, 1, 4};
+    P.X = X;
+    P.Y = Y;
+    PARAMETERS.LENGTH1 = -3;
+    //LIC_0 should fail because LENGTH1 is less than 0
+    boolean result = LIC_0();
+    assert(result == 0);
+    return result; 
+}
+
 
 //function to call the LICs test cases and prints the result
 test_all_LIC()
-{
+{   //LIC0 test cases call 
    if (LIC_0_Fail_case1() == 0) 
    printf("LIC_0 test case 1 for failing input : PASSED \n");
    else
    printf("LIC_0 test case 1 for failing input : FAILED \n");
+
+
+   if (LIC_0_Fail_case2() == 0) 
+   printf("LIC_0 test case 2 for failing input : PASSED \n");
+   else
+   printf("LIC_0 test case 2 for failing input : FAILED \n");
  
 }
 
