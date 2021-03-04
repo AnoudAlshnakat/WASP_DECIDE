221cb3fb58d8dffc3af309bf47b56eaa24b452d8
AnoudAlshnakat <ano.o@live.com>
2021-03-04 13:39:23 +0100

    test: LIC0 passing case

--- test_requierments.c
+++ test_requierments.c
@@ -3,6 +3,21 @@
 #include <string.h>
 #include <assert.h>
 
+boolean LIC_0_Pass_case1()
+{
+    //test inputs: Number of points = 10, the parameter LENGTH1 = 4 unit, and coordinates of 10 points.
+    NUMPOINTS = 10;
+    double X[] = {5, 3, 5, 8, 5, 3, 5, 8, 2, 3};
+    double Y[] = {1, 4, 3, 1, 5, 6, 1, 6, 1, 4};
+    P.X = X;
+    P.Y = Y;
+    PARAMETERS.LENGTH1 = 4;
+    //LIC_0 should pass because there is a distance between two consequent points
+    //on a distance that are larger than LENGTH1
+    boolean result = LIC_0();
+    assert(result == 1);
+    return result; 
+}
 
 boolean LIC_0_Fail_case1()
 {
@@ -20,6 +35,7 @@ boolean LIC_0_Fail_case1()
     assert(result == 0);
     return result; 
 }
+
 
 boolean LIC_0_Fail_case2()
 {
@@ -40,6 +56,13 @@ boolean LIC_0_Fail_case2()
 //function to call the LICs test cases and prints the result
 test_all_LIC()
 {   //LIC0 test cases call 
+
+   if (LIC_0_Pass_case1() == 1) 
+   printf("LIC_0 test case 1 for passing input : PASSED \n");
+   else
+   printf("LIC_0 test case 1 for passing input : FAILED \n");
+
+
    if (LIC_0_Fail_case1() == 0) 
    printf("LIC_0 test case 1 for failing input : PASSED \n");
    else
